409ad1b3e1f1178c7334b0f4a4655d4cf93c0935
Max <max@tfq.at>
2023-05-19 21:18:51 +0200

    remove csp head

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,14 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { NextStrictCSP } from 'next-strict-csp'
 
-const HeadCSP = process.env.NODE_ENV === 'production' ? NextStrictCSP : Head
 export default function Document() {
     return (
         <Html lang='en'>
-            <HeadCSP>
-                {process.env.NODE_ENV === 'production' &&
-                    <meta httpEquiv="Content-Security-Policy" />}
-            </HeadCSP>
             <body style={{margin: 0, padding: 0}}>
                 <Main />
                 <NextScript />

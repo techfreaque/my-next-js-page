0d1d7381b6159da777ad5eede397f0a99db8b54b
Max <max@tfq.at>
2023-05-20 16:12:08 +0200

    improve footer fanciness

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Layout} from 'antd';
 import {GithubOutlined} from '@ant-design/icons';
+import { getFancyColorsStyle } from './colorPalette';
 
 const {Footer} = Layout;
 
 export default function FooterContent() {
-    return (
-        <Footer style={
-            {textAlign: 'center'}
+    const [isHover, setIsHover] = useState(false);
+    return (<Footer style={
+        {textAlign: 'center'}
+    }>
+        made with next.js - by Marcus Brandstaetter ©2023
+        <a href={"https://github.com/techfreaque/my-next-js-page"}
+            target="_blank"
+            style={
+                {marginLeft: "10px"}
+            }
+            onMouseEnter={
+                () => setIsHover(true)
+            }
+            onMouseLeave={
+                () => setIsHover(false)
         }>
-            made with next.js - by Marcus Brandstaetter ©2023
-            <a href={"https://github.com/techfreaque/my-next-js-page"}
-                target="_blank" style={{marginLeft: "10px"}} >
 
-                <Button icon={
-                    (
-                        <GithubOutlined/>)
-                }>
-                    Fork this page on GitHub
-                </Button>
-            </a>
-        </Footer>
-    );
+            <Button icon={
+                (<GithubOutlined/>)
+            }>
+                Fork this page on GitHub
+            </Button>
+            <span style={
+                {
+                    marginLeft: "10px",
+                    ...getFancyColorsStyle(isHover)
+                }
+            }> {"leave me a star while you're there "} </span>
+        </a>
+
+    </Footer>);
 }

7a682af17d13ce5b6c40011343ae52cc58596f55
Max B <max@tfq.at>
2025-11-25 14:34:27 +0100

    improve deploy script

--- docker_install_and_update.sh
+++ docker_install_and_update.sh
@@ -1,7 +1,6 @@
 git stash
 git pull
 chmod u+x docker_install_and_update.sh
-docker build . -t my-next-js-page
-docker stop my-next-js-page
-docker rm my-next-js-page
-docker run -d --name my-next-js-page --network caddy_internal my-next-js-page
+docker compose down
+docker compose build
+docker compose up -d
